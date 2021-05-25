a431f1b2cde69bbe4ef8ef18eea24b0a28d0000a
Naveen Kumar <52369168+NaveenKumar9717@users.noreply.github.com>
2021-05-25 15:41:17 +0530

    Update passport.js

--- config/passport.js
+++ config/passport.js
@@ -8,7 +8,7 @@ module.exports = function(passport){
         callbackURL : '/auth/google/callback'
     },
     async (accesToken, refreshToken,  profile, done) =>{
-        console.log(profile)
+        
         const newUser = {
             googleId : profile.id,
             displayName : profile.displayName,
